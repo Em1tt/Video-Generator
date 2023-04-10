3db6a91e706ac258b7f939a01b0ff056d6c6ca0f
Richard <55552821+Em1tt@users.noreply.github.com>
2023-04-10 19:17:02 +0200

    Cleanup, create README.md, Fix issues

---
+++ README.md
@@ -0,0 +1,35 @@
+# Video Compilation Generator
+
+This utility fetches videos from a preconfigured subreddit, and creates a compilation out of them. It utilizes a queue system, which helps prevent lag on your device.
+
+To start using this utility, run:
+
+```bash
+npm install
+```
+
+**Make sure your device has FFmpeg installed on it**  
+Not sure how to install FFmpeg? [[Windows]](https://phoenixnap.com/kb/ffmpeg-windows) [[Linux]](https://phoenixnap.com/kb/install-ffmpeg-ubuntu)
+
+Running the code: 
+```bash
+node .
+#or
+node index.js
+```
+
+## Default config.json
+```json
+{
+    "subreddit": "escapefromtarkov",
+    "filter": "top",
+    "outputDir": "",
+    "tmpDir": "./tmp/",
+    "amount": 25,
+    "resolution": "1920x?",
+    "aspect": "16:9",
+    "showAuthors": true,
+    "cleanCache": true,
+    "silent": false
+}
+```
--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-const { subreddit, filter } = require("./config.json");
+const {
+  subreddit,
+  filter,
+  outputDir,
+  tmpDir,
+  amount,
+  resolution,
+  aspect,
+  showAuthors,
+  cleanCache,
+  silent
+} = require("./config.json");
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 const fs = require("fs");
-const glob = require("glob");
-const { exec } = require("child_process");
-
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 const ffmpeg = require("fluent-ffmpeg");
-
+const crypto = require("crypto");
 const videos = [];
 let after = null;
-
 (async () => {
-  while (videos.length < 50) {
+  while (videos.length < amount) {
     await fetch(
-      `https://www.reddit.com/r/escapefromtarkov/top/.json?t=week${
+      `https://www.reddit.com/r/${subreddit}/${filter}/.json?t=week${
         after ? `&after=${after}` : ``
       }`
     ).then(async (res) => {
@@ -23,36 +32,81 @@ let after = null;
         if (post.data.is_video && post.data.is_reddit_media_domain) return true;
       });
       for (const post of posts) {
+        if (videos.length > amount) break;
         const id = post.data?.url_overridden_by_dest?.split(".it/")[1];
         const videoURL = post.data.media?.reddit_video.fallback_url;
         const audioURL = `https://v.redd.it/${id}/DASH_audio.mp4?source=fallback`;
-        await processVideoSync(videoURL, audioURL, id);
+        silent ? 0 : console.log(`STARTED VIDEO: ${id}.mp4`);
+        const author = `u/${post.data.author}`;
+        await processVideoSync(videoURL, audioURL, id, author);
       }
     });
   }
-
-  let cmd = `ffmpeg -i "concat:${videos.join("|")}" -c:a copy -c:v copy merged_video.avi`
-    exec(cmd, function(err, stdout, stderr) {
-  if(err) console.log(err)
-  else console.log("Done!")
-})
-
+  await mergeVideoSync();
+  if (cleanCache) {
+    fs.rmSync(tmpDir, { recursive: true, force: true });
+    fs.mkdirSync(tmpDir);
+    silent ? 0 : console.log("CLEANED TEMP FOLDER");
+  }
+  silent ? 0 : console.log("COMPLETED.");
 })();
 
-async function processVideoSync(videoURL, audioURL, id) {
+async function mergeVideoSync() {
+  //Randomized due to FFMPEG asking for permission to overwrite - lazy fix ik
+  const videoName = crypto.randomUUID();
+  silent ? 0 : console.log(`STARTED COMPILATION: ${videoName}.mp4`);
   return new Promise((resolve, reject) => {
-    if (fs.existsSync(`./tmp/output/${id}.avi`)) {
-      videos.push(`./tmp/output/${id}.avi`);
+    fs.writeFile(
+      `${tmpDir}list.txt`,
+      videos
+        .map((vid) => {
+          return `file '${vid}'`;
+        })
+        .join("\n"),
+      async (err) => {
+        await exec(
+          `ffmpeg -safe 0 -f concat -i ${tmpDir}list.txt -c copy ${outputDir}${videoName}.mp4`
+        );
+        silent ? 0 : console.log(`FINISHED COMPILATION: ${videoName}.mp4`);
+        return resolve(videoName);
+      }
+    );
+  });
+}
+
+async function processVideoSync(videoURL, audioURL, id, author) {
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(`${tmpDir}${id}.mp4`)) {
+      videos.push(`${id}.mp4`);
+      silent ? 0 : console.log(`IGNORED VIDEO: ${id}.mp4\n---------------------`);
       return resolve();
-    };
+    }
     const proc = new ffmpeg();
+
     proc
-      .addInput(videoURL).size('1920x?').autoPad()
+      .addInput(videoURL)
+      .size(resolution)
+      .aspect(aspect)
+      .autoPad()
+      .videoFilters({
+        filter: "drawtext",
+        options: {
+          //fontfile: "",
+          text: showAuthors ? author : "",
+          fontsize: "(w/40)",
+          fontcolor: "white",
+          bordercolor: "black",
+          borderw: 3,
+          x: "(w-text_w-10)",
+          y: "(h-text_h-10)",
+          /* etc. */
+        },
+      })
       .addInput(audioURL)
-      .output(`./tmp/output/${id}.avi`)
+      .output(`${tmpDir}${id}.mp4`)
       .on("end", () => {
-        videos.push(`./tmp/output/${id}.avi`);
-        console.log(`FINISHED VIDEO #${videos.length}`);
+        videos.push(`${id}.mp4`);
+        silent ? 0 : console.log(`FINISHED VIDEO: ${id}.mp4\n---------------------`);
         resolve();
       })
       .on("error", () => {
